b156c135369f6eef3943fd5055935c9c942f6e44
takaides <takaides+github@gmail.com>
2017-08-26 22:08:55 -0400

    Initial boilerplate commit

---
+++ src/com/theironyard/java/interfaceproject/Main.java
@@ -0,0 +1,8 @@
+package com.theironyard.java.interfaceproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/theironyard/java/interfaceproject/Solider.java
@@ -0,0 +1,14 @@
+package com.theironyard.java.interfaceproject;
+
+public class Solider {
+//
+//    You are tasked with writing the code for the mindless (and under appreciated) common soldiers.
+//
+//    Write an abstract Soldier class with a few instance variables (e.g. name, rank, serialNumber). Write a few common methods for the behavior (e.g. sleep, eat, walk) and just return a String for something meaning (e.g. "snore", "chew", "trudge").
+//
+//    Write an abstract method speak which returns a String. Each class that extends the FootSoldier class will have to implement the method (e.g. Marines could return "oohrah", Army could return "Hooah", Navy could return "Hooyah", ...). All of the methods you're writing will work this way; you can think of them as sound effects (punch => "Blamo!" or whatever you pick).
+//
+//    The way the soldier fights is by implementing interfaces. Create a couple different interfaces with appropriate methods for different kind of fighting. Have the methods return String.
+//
+//    Of course you have to test your code. Create a couple classes that extend Soldier and implement one or more interfaces. When implementing methods return something appropriate for the method. In the main method of Main call the methods all the method of the Soldier class and the interface(s). Simply write to the console the Strings that are returned.
+}
